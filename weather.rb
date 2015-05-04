6a01d2698ad4d2637f23f78fb8cac3a9df889008
Charles Okara <cgokara@gmail.com>
2015-05-04 00:30:10 -0400

    Using DC

--- weather.rb
+++ weather.rb
@@ -1,4 +1,4 @@
-response = HTTParty.get("http://weather.yahooapis.com/forecastrss?w=2347058")
+response = HTTParty.get("http://weather.yahooapis.com/forecastrss?w=12765842")
 temp = Nokogiri::XML(response.body).css('yweather|condition').first.attributes['temp'].value
 descr = Nokogiri::XML(response.body).css('yweather|condition').first.attributes['text'].value
 
